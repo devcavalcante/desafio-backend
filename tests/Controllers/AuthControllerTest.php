aa1e36dc4dc3b4e4135afe3428e28a3f05926338
debora.brito <deehsccp@hotmail.com>
2021-07-03 15:58:56 -0300

    add tests

---
+++ tests/Controllers/AuthControllerTest.php
@@ -0,0 +1,65 @@
+<?php
+
+
+// namespace Controllers;
+
+use App\Models\User;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\TestCase;
+
+class AuthControllerTest extends TestCase
+{
+    // use DatabaseMigrations;
+
+    public function createApplication()
+    {
+        return require './bootstrap/app.php';
+    }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testUserCanLoginWithCorrectCredentials()
+    {
+        $this->artisan('passport:install');
+        $user = [ 
+            'email' => 'lojista@gmail.com',
+            'password' => 'midias123'
+        ];
+
+
+        $request = $this->post(route('login'), $user);
+        $request->assertResponseStatus(200);
+        $request->seeJsonStructure(['access_token','expires_at', 'user']);
+    }
+
+    public function testUserCannotLoginWithWrongPassword()
+    {
+
+        $user = User::factory()->create();
+        $normal = [
+            'email' => $user->email,
+            'password' => 'senhaerrada'
+        ];
+
+        $request = $this->post(route('login'), $normal);
+        $request->assertResponseStatus(401);
+        $request->seeJson(['errors' => 'Invalid credentials']);
+    }
+
+    public function testUserCannotLoginWithEmailThatDoesNotExist()
+    {
+        $created = User::factory()->create();
+        $user = [
+            'email' => 'abc@mail.com',
+            'password' => $created->password,
+        ];
+
+        $request = $this->post(route('login'), $user);
+        $request->assertResponseStatus(401);
+        $request->seeJson(['errors' => 'Invalid credentials']);
+    }
+
+}
