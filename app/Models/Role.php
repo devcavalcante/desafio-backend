dfd9b5c8c2e315302da960b590b0f2d65cf91a81
debora.brito <deehsccp@hotmail.com>
2021-07-05 00:58:43 -0300

    add false incrementing

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,15 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    use HasFactory;
+    
+    public $incrementing = false;
 
     protected $fillable = [
-        'type',
+        'id', 'type'
     ];
 
     public function user()
